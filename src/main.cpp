60b5188b750979c31dcf13547bf4bdb4a2d9a22f
Lele <khoi.lenguyen3010@hcmut.edu.vn>
2026-01-04 19:05:13 +0700

    Add bluetooth function (just in case)

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,14 @@ BluetoothSerial SerialBT;
 QRCode qrcode;
 Servo myServo;
 
+static bool btScanAsync = true;
+static bool btScanSync = true;
+
 int lastState = -1; 
+
+void btAdvertisedDeviceFound(BTAdvertisedDevice *pDevice) {
+  Serial.printf("Found a device asynchronously: %s\n", pDevice->toString().c_str());
+}
 
 void drawQrToTFT(String text) {
   tft.fillScreen(ST7735_BLACK);
@@ -67,9 +74,31 @@ void setup()
 {
   Serial.begin(115200); 
 
-  SerialBT.begin("ESP32_Lele");
-  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
-  
+   SerialBT.begin("ESP32_Lele");  
+  Serial.println("The device started, now you can pair it with bluetooth!");
+
+  if (btScanAsync) {
+    Serial.print("Starting asynchronous discovery... ");
+    if (SerialBT.discoverAsync(btAdvertisedDeviceFound)) {
+      Serial.println("Findings will be reported in \"btAdvertisedDeviceFound\"");
+      delay(10000);
+      Serial.print("Stopping discoverAsync... ");
+      SerialBT.discoverAsyncStop();
+      Serial.println("stopped");
+    } else {
+      Serial.println("Error on discoverAsync f.e. not working after a \"connect\"");
+    }
+  }
+
+  if (btScanSync) {
+    Serial.println("Starting synchronous discovery... ");
+    BTScanResults *pResults = SerialBT.discover(BT_DISCOVER_TIME);
+    if (pResults) {
+      pResults->dump(&Serial);
+    } else {
+      Serial.println("Error on BT Scan, no result!");
+    }
+  }
 
   tft.initR(INITR_BLACKTAB);
   tft.fillScreen(ST77XX_BLACK);
